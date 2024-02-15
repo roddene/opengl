cd0aa2e236cef9287064bd3ce702f01c3c061bf9
roddene <44795682+roddene@users.noreply.github.com>
2024-02-14 23:11:53 -0500

    init

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./include/glad/glad.h"
+#define SCREENWIDTH 640
+#define SCREENHEIGHT 480
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include<sstream>
+#include <string>
+#include <vector>
+
